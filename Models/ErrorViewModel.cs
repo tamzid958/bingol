c0e93c08b53dd60510d833b38b4a97d4ce201659
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-01-04 04:32:39 +0600

    target sdk 5

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bingol.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
