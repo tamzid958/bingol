6af5071b5f4d1c1a91ce998e1c689198bce58350
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-01-14 17:55:37 +0600

    Dashboard added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,3 +8,49 @@ $('.owl-carousel').owlCarousel({
     autoWidth: true,
     items: 4
 })
+
+// Graphs
+var ctx = $('#myChart');
+// eslint-disable-next-line no-unused-vars
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+        ],
+        datasets: [{
+            data: [
+                15339,
+                21345,
+                18483,
+                24003,
+                23489,
+                24092,
+                12034
+            ],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#EDAB06',
+            borderWidth: 4,
+            pointBackgroundColor: '#E6E6FA'
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
+    }
+})
